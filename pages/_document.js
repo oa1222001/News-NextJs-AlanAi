3480ad1e5838df4b467ead3249944f451b9b0ef5
Omar Allam <oa1222001@gmail.com>
2022-04-27 14:02:53 +0200

    Final

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta charset="utf-8" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import NewsCards from "../public/components/NewsCards/NewsCards";
+
 // i write my enviroment variables at vercel config.
 import wordsToNumbers from "words-to-numbers";
 import useStyles from "../styles/HomeStyle";
+import Head from "next/head";
 export default function Home() {
   const [newsArticles, setNewsArticles] = useState([]);
   const [activeArt, setActiveArt] = useState(-1);
@@ -35,15 +37,26 @@ export default function Home() {
     });
   }, []);
   return (
-    <div>
-      <div className={classes.logoContainer}>
-        <img
-          src={"	https://alan.app/static/alan-logo-medium.79f960a7.svg"}
-          className={classes.alanLogo}
-          alt="alan logo"
+    <Fragment>
+      <Head>
+        <meta
+          name="description"
+          content="News Website uses sound to find news from any source."
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="https://img.icons8.com/color/344/news.png" />
+        <title>News App</title>
+      </Head>
+      <div>
+        <div className={classes.logoContainer}>
+          <img
+            src={"	https://alan.app/static/alan-logo-medium.79f960a7.svg"}
+            className={classes.alanLogo}
+            alt="alan logo"
+          />
+        </div>
+        <NewsCards articles={newsArticles} activeArt={activeArt} />
       </div>
-      <NewsCards articles={newsArticles} activeArt={activeArt} />
-    </div>
+    </Fragment>
   );
 }
