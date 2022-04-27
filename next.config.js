0698d904eb40c88d4e4e5776dacf53810f75f95b
Omar Allam <oa1222001@gmail.com>
2022-04-27 14:11:35 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-  env: {
-    alan_key:
-      "38eb5a37a6ac309b4d6430a3e0a193792e956eca572e1d8b807a3e2338fdd0dc/stage",
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
